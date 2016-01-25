501b9a45e055fcb5d640eb0675512dfe4eb9e08f
gcollioucloren <gcollioucloren@enseirb-matmeca.fr>
2016-01-25 09:26:18 +0100

    ALLELUIA

--- src/StockChangeAnalysis.java
+++ src/StockChangeAnalysis.java
@@ -15,33 +15,18 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 public class StockChangeAnalysis {
 
     public static class StockChangeAnalysisMapper
-            extends Mapper<Object, Text, IntWritable, Writable>{
+            extends Mapper<Object, Text, IntWritable, IntWritable>{
 
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
-            String tokens[] = value.toString().split(",");
-            if (tokens.length < 7 || tokens[4].length()==0) return;
-            String name;
-            int region;
-            int population;
-            double latitude;
-            double longitude;
-            try {
-                name = tokens[2];
-                region = Integer.parseInt(tokens[3]);
-                population = Integer.parseInt(tokens[4]);
-                latitude = Double.parseDouble(tokens[5]);
-                longitude = Double.parseDouble(tokens[6]);
-            }
-            catch(Exception e) {
-                return;
-            }
-            context.write(new IntWritable(region) , new MarketIndex());
+            System.out.println("TESTTEST" + " " + value + "\n\n\n" +
+                    "\n\n");
+            context.write(new IntWritable(1) , new IntWritable(2));
         }
 
     }
 
-    public static class StockChangeAnalysisCombiner extends Reducer<IntWritable, Writable, IntWritable, Writable> {
+    /*public static class StockChangeAnalysisCombiner extends Reducer<IntWritable, Writable, IntWritable, Writable> {
         public void reduce(IntWritable key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException {
             int sum = 0;
             for (IntWritable val : values) {
@@ -49,7 +34,7 @@ public class StockChangeAnalysis {
             }
             context.write(key, new IntWritable(sum));
         }
-    }
+    }*/
 
     public static class StockChangeAnalysisReducer
             extends Reducer<Text,IntWritable,Text,IntWritable> {
@@ -61,18 +46,22 @@ public class StockChangeAnalysis {
     }
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
+
+        conf.set("xmlinput.start", "<tbody>");
+        conf.set("xmlinput.end", "</tbody>");
+
         Job job = Job.getInstance(conf, "MonProg");
+        job.setInputFormatClass(XmlInputFormat.class);
         job.setNumReduceTasks(1);
         job.setJarByClass(StockChangeAnalysis.class);
         job.setMapperClass(StockChangeAnalysisMapper.class);
         job.setMapOutputKeyClass(IntWritable.class);
-        job.setMapOutputValueClass(Writable.class);
-        job.setCombinerClass(StockChangeAnalysisCombiner.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        //job.setCombinerClass(StockChangeAnalysisCombiner.class);
         job.setReducerClass(StockChangeAnalysisReducer.class);
         job.setOutputKeyClass(IntWritable.class);
         job.setOutputValueClass(Writable.class);
         job.setOutputFormatClass(TextOutputFormat.class);
-        job.setInputFormatClass(TextInputFormat.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
