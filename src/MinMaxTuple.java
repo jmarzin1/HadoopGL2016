e373fc3b46bc061201f6b702dab5c91aef254429
gcollioucloren <gcollioucloren@enseirb-matmeca.fr>
2016-01-26 13:18:24 +0100

    On rajoute un min max/ On sait pas trop pourquoi

---
+++ src/MinMaxTuple.java
@@ -0,0 +1,49 @@
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.Date;
+
+public class MinMaxTuple implements Writable{
+    private float minVar;
+    private float maxVar;
+
+    public float getMinVar() {
+        return minVar;
+    }
+
+    public void setMinVar(float minVar) {
+        this.minVar = minVar;
+    }
+
+    public float getMaxVar() {
+        return maxVar;
+    }
+
+    public void setMaxVar(float maxVar) {
+        this.maxVar = maxVar;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeFloat(minVar);
+        dataOutput.writeFloat(maxVar);
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        if (dataInput != null) {
+            minVar = dataInput.readFloat();
+            maxVar = dataInput.readFloat();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "MinMaxTuple{" +
+                ", minVar=" + minVar +
+                ", maxVar=" + maxVar +
+                '}';
+    }
+}
--- src/StockChangeAnalysis.java
+++ src/StockChangeAnalysis.java
@@ -87,6 +87,57 @@ public class StockChangeAnalysis {
 
     }
 
+
+    public static class MinMaxCountMapper extends Mapper<Object, Text, Text, MinMaxTuple> {
+        private Text outUserId = new Text();
+        private MinMaxTuple outminMaxTuple = new MinMaxTuple();
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String fileName = ((FileSplit) context.getInputSplit()).getPath().getName();
+            Date date = convertDate(fileName);
+            MarketIndex marketIndex = new MarketIndex();
+            marketIndex.setDate(date);
+            String toParse = value.toString();
+            String[] tokens = toParse.split("\n");
+            if ((tokens.length >= 2) && tokens[1].contains("tdv-var")) {
+                int i = 2;
+                while (!tokens[i].contains("</tr>")) {
+                    String[] lineTokens = tokens[i].split(">");
+                    parseLine(lineTokens, marketIndex);
+                    i++;
+                }
+            } else {
+                return;
+            }
+            outminMaxTuple.setMinVar(marketIndex.getDailyVariation());
+            outminMaxTuple.setMaxVar(marketIndex.getDailyVariation());
+            outUserId.set(marketIndex.getName());
+            context.write(outUserId, outminMaxTuple);
+        }
+    }
+
+
+    public static class MinMaxCountReducer extends Reducer<Text, MinMaxTuple, Text, MinMaxTuple> {
+        private MinMaxTuple result = new MinMaxTuple();
+
+        public void reduce(Text key, Iterable<MinMaxTuple> values,
+                           Context context) throws IOException, InterruptedException {
+            result.setMinVar(0);
+            result.setMaxVar(0);
+            for (MinMaxTuple val : values) {
+                if (val.getMinVar() < (result.getMinVar())) {
+                    result.setMinVar(val.getMinVar());
+                }
+                if (val.getMaxVar() > result.getMaxVar()) {
+                    result.setMaxVar(val.getMaxVar());
+                }
+            }
+            System.out.println(key + " " + result);
+            context.write(key, result);
+        }
+    }
+
+
+
     /*public static class StockChangeAnalysisCombiner extends Reducer<IntWritable, Writable, IntWritable, Writable> {
         public void reduce(IntWritable key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException {
             int sum = 0;
@@ -145,6 +196,7 @@ public class StockChangeAnalysis {
         }
     }
 
+
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
 
@@ -159,45 +211,54 @@ public class StockChangeAnalysis {
         //FileInputFormat.addInputPath(job, new Path(args[0]));
         //FileOutputFormat.setOutputPath(job, new Path(args[1]));
         String commande = "";
-		if (args.length>2) {
-			commande = args[0];
-			FileInputFormat.addInputPath(job, new Path(args[1]));
-			FileOutputFormat.setOutputPath(job, new Path(args[2]));
-		}
-		int returnCode = 0;
-		switch (commande) {
-		case "clean":
-			//job.setMapperClass(CitiesWithPopMapper.class);
-			//job.setReducerClass(MaxReduce.class);
-			//returnCode = job.waitForCompletion(true) ? 0 : 1;
-			break;
-		case "parsing":
-			job.setMapperClass(StockChangeAnalysisMapper.class);
-			job.setMapOutputKeyClass(IntWritable.class);
-			job.setMapOutputValueClass(MarketIndex.class);
-			job.setReducerClass(StockChangeAnalysisReducer.class);
-			job.setOutputKeyClass(IntWritable.class);
-	        job.setOutputValueClass(MarketIndex.class);
-			returnCode = job.waitForCompletion(true) ? 0 : 1;
-			break;
-		case "topK":
-			job.setMapperClass(StockChangeTopKMapper.class);
-			job.setMapOutputKeyClass(NullWritable.class);
-			job.setMapOutputValueClass(MarketIndex.class);
-			job.setReducerClass(StockChangeTopKReducer.class);
-			job.setOutputKeyClass(NullWritable.class);
-	        job.setOutputValueClass(Text.class);
-			returnCode = job.waitForCompletion(true) ? 0 : 1;
-			break;
-		default:
-			System.out.println("Usage: commands args");
-			System.out.println("commands:");
-			System.out.println(" - parsing [inputURI] [outputURI]");
-			System.out.println(" - topK [inputURI] [outputURI]");
-			returnCode = 1;
-		}
-		
-		System.exit(returnCode);
+        if (args.length > 2) {
+            commande = args[0];
+            FileInputFormat.addInputPath(job, new Path(args[1]));
+            FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        }
+        int returnCode = 0;
+        switch (commande) {
+            case "clean":
+                //job.setMapperClass(CitiesWithPopMapper.class);
+                //job.setReducerClass(MaxReduce.class);
+                //returnCode = job.waitForCompletion(true) ? 0 : 1;
+                break;
+            case "parsing":
+                job.setMapperClass(StockChangeAnalysisMapper.class);
+                job.setMapOutputKeyClass(IntWritable.class);
+                job.setMapOutputValueClass(MarketIndex.class);
+                job.setReducerClass(StockChangeAnalysisReducer.class);
+                job.setOutputKeyClass(IntWritable.class);
+                job.setOutputValueClass(MarketIndex.class);
+                returnCode = job.waitForCompletion(true) ? 0 : 1;
+                break;
+            case "topK":
+                job.setMapperClass(StockChangeTopKMapper.class);
+                job.setMapOutputKeyClass(NullWritable.class);
+                job.setMapOutputValueClass(MarketIndex.class);
+                job.setReducerClass(StockChangeTopKReducer.class);
+                job.setOutputKeyClass(NullWritable.class);
+                job.setOutputValueClass(Text.class);
+                returnCode = job.waitForCompletion(true) ? 0 : 1;
+                break;
+            case "minMax":
+                job.setMapperClass(MinMaxCountMapper.class);
+                job.setMapOutputKeyClass(Text.class);
+                job.setMapOutputValueClass(MinMaxTuple.class);
+                job.setReducerClass(MinMaxCountReducer.class);
+                job.setOutputKeyClass(Text.class);
+                job.setOutputValueClass(MinMaxTuple.class);
+                returnCode = job.waitForCompletion(true) ? 0 : 1;
+                break;
+            default:
+                System.out.println("Usage: commands args");
+                System.out.println("commands:");
+                System.out.println(" - parsing [inputURI] [outputURI]");
+                System.out.println(" - topK [inputURI] [outputURI]");
+                returnCode = 1;
+        }
+
+        System.exit(returnCode);
     }
 
     public static void parseLine(String[] lineTokens, MarketIndex marketIndex) {
