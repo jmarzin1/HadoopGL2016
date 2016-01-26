63d36aab0d88a2dc00cc8acdab93d4e651cc4938
gcollioucloren <gcollioucloren@enseirb-matmeca.fr>
2016-01-26 10:00:31 +0100

    TopKKKKK

--- src/StockChangeAnalysis.java
+++ src/StockChangeAnalysis.java
@@ -21,8 +21,36 @@ public class StockChangeAnalysis {
 
     public static class StockChangeAnalysisMapper
             extends Mapper<Object, Text, NullWritable, MarketIndex> {
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            String fileName = ((FileSplit) context.getInputSplit()).getPath().getName();
+            Date date = convertDate(fileName);
+            MarketIndex marketIndex = new MarketIndex();
+            marketIndex.setDate(date);
+            String toParse = value.toString();
+            String[] tokens = toParse.split("\n");
+            if ((tokens.length >= 2) && tokens[1].contains("tdv-var")) {
+                int i = 2;
+                while (!tokens[i].contains("</tr>")) {
+                    String[] lineTokens = tokens[i].split(">");
+                    parseLine(lineTokens, marketIndex);
+                    i++;
+                }
+            }
+            else {
+                return;
+            }
+            context.write(NullWritable.get(), marketIndex);
+
+        }
+    }
+
+
+    public static class StockChangeTopKMapper
+            extends Mapper<Object, Text, NullWritable, MarketIndex> {
         public int k = 10;
-        private TreeMap<Integer, MarketIndex> topKMarketIndexes = new TreeMap<Integer, MarketIndex>();
+        private TreeMap<Integer, MarketIndex> topKMarketIndexes = new TreeMap<>();
 
         public void map(Object key, Text value, Context context
         ) throws IOException, InterruptedException {
@@ -55,71 +83,6 @@ public class StockChangeAnalysis {
             // Output our ten records to the reducers with a null key
             for (MarketIndex marketIndex : topKMarketIndexes.values()) {
                 context.write(NullWritable.get(), marketIndex);
-            }
-        }
-
-        private Date convertDate(String fileName) {
-        	String pattern = "([0-9]+)";
-        	long timestamp = 0;
-        	Scanner scan = new Scanner(fileName);
-        	if (scan.findInLine(pattern) != null) {
-        		MatchResult matchResult = scan.match();
-        		timestamp = Long.parseLong(matchResult.group(0))*1000;
-        		scan.close();
-        	}
-			return new Date(timestamp);
-		}
-
-		private void parseLine(String[] lineTokens, MarketIndex marketIndex) {
-            String pattern = "(tdv-[A-Za-z_]+)";
-            String[] localTokens;
-            Scanner scan = new Scanner(lineTokens[0]);
-            if (scan.findInLine(pattern) != null) {
-                MatchResult matchResult = scan.match();
-                switch (matchResult.group(0)) {
-                    case "tdv-libelle":
-                        localTokens = lineTokens[4].split("<");
-                        marketIndex.setName(localTokens[0]);
-                        break;
-                    case "tdv-last":
-                        localTokens = lineTokens[3].split("\\s");
-                        marketIndex.setClosingValue(Float.parseFloat(localTokens[0]));
-                        break;
-                    case "tdv-var":
-                        localTokens = lineTokens[2].split("%");
-                        marketIndex.setDailyVariation(Float.parseFloat(localTokens[0]));
-                        break;
-                    case "tdv-open":
-                        localTokens = lineTokens[3].split("<");
-                        if(!localTokens[0].equals("ND")) {
-                            marketIndex.setOpeningValue(Float.parseFloat(localTokens[0].replaceAll("\\s+","")));
-                        }
-                        break;
-                    case "tdv-high":
-                        localTokens = lineTokens[3].split("<");
-                        if(!localTokens[0].equals("ND")) {
-                            marketIndex.setHigherValue(Float.parseFloat(localTokens[0].replaceAll("\\s+","")));
-                        }
-                        break;
-                    case "tdv-low":
-                        localTokens = lineTokens[3].split("<");
-                        if(!localTokens[0].equals("ND")) {
-                            marketIndex.setLowerValue(Float.parseFloat(localTokens[0].replaceAll("\\s+","")));
-                        }
-                        break;
-                    case "tdv-var_an":
-                        localTokens = lineTokens[2].split("%");
-                        marketIndex.setAnnualVariation(Float.parseFloat(localTokens[0]));
-                        break;
-                    case "tdv-tot_volume":
-                        localTokens = lineTokens[2].split("<");
-                        int volumeTotal = Integer.parseInt(localTokens[0].replaceAll("\\s+",""));
-                        marketIndex.setCapitalization(volumeTotal);
-                        break;
-                    default:
-                        break;
-                }
-                scan.close();
             }
         }
 
@@ -159,7 +122,7 @@ public class StockChangeAnalysis {
 				}
 			}
 			for (MarketIndex mi : topKMarketIndexes.descendingMap().values()) {
-                System.out.println(topKMarketIndexes.size() + " c'ets la taille");
+                System.out.println(mi);
 				context.write(key, mi);
 			}
 		}
@@ -174,10 +137,9 @@ public class StockChangeAnalysis {
         Job job = Job.getInstance(conf, "MonProg");
         job.setNumReduceTasks(1);
         job.setJarByClass(StockChangeAnalysis.class);
-        job.setMapperClass(StockChangeAnalysisMapper.class);
+        job.setMapperClass(StockChangeTopKMapper.class);
         job.setMapOutputKeyClass(NullWritable.class);
         job.setMapOutputValueClass(MarketIndex.class);
-        //job.setCombinerClass(StockChangeAnalysisCombiner.class);
         job.setReducerClass(StockChangeTopKReducer.class);
         job.setOutputKeyClass(NullWritable.class);
         job.setOutputValueClass(MarketIndex.class);
@@ -188,5 +150,69 @@ public class StockChangeAnalysis {
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 
+    public static void parseLine(String[] lineTokens, MarketIndex marketIndex) {
+        String pattern = "(tdv-[A-Za-z_]+)";
+        String[] localTokens;
+        Scanner scan = new Scanner(lineTokens[0]);
+        if (scan.findInLine(pattern) != null) {
+            MatchResult matchResult = scan.match();
+            switch (matchResult.group(0)) {
+                case "tdv-libelle":
+                    localTokens = lineTokens[4].split("<");
+                    marketIndex.setName(localTokens[0]);
+                    break;
+                case "tdv-last":
+                    localTokens = lineTokens[3].split("\\s");
+                    marketIndex.setClosingValue(Float.parseFloat(localTokens[0]));
+                    break;
+                case "tdv-var":
+                    localTokens = lineTokens[2].split("%");
+                    marketIndex.setDailyVariation(Float.parseFloat(localTokens[0]));
+                    break;
+                case "tdv-open":
+                    localTokens = lineTokens[3].split("<");
+                    if(!localTokens[0].equals("ND")) {
+                        marketIndex.setOpeningValue(Float.parseFloat(localTokens[0].replaceAll("\\s+","")));
+                    }
+                    break;
+                case "tdv-high":
+                    localTokens = lineTokens[3].split("<");
+                    if(!localTokens[0].equals("ND")) {
+                        marketIndex.setHigherValue(Float.parseFloat(localTokens[0].replaceAll("\\s+","")));
+                    }
+                    break;
+                case "tdv-low":
+                    localTokens = lineTokens[3].split("<");
+                    if(!localTokens[0].equals("ND")) {
+                        marketIndex.setLowerValue(Float.parseFloat(localTokens[0].replaceAll("\\s+","")));
+                    }
+                    break;
+                case "tdv-var_an":
+                    localTokens = lineTokens[2].split("%");
+                    marketIndex.setAnnualVariation(Float.parseFloat(localTokens[0]));
+                    break;
+                case "tdv-tot_volume":
+                    localTokens = lineTokens[2].split("<");
+                    int volumeTotal = Integer.parseInt(localTokens[0].replaceAll("\\s+",""));
+                    marketIndex.setCapitalization(volumeTotal);
+                    break;
+                default:
+                    break;
+            }
+            scan.close();
+        }
+    }
+
+    public static Date convertDate(String fileName) {
+        String pattern = "([0-9]+)";
+        long timestamp = 0;
+        Scanner scan = new Scanner(fileName);
+        if (scan.findInLine(pattern) != null) {
+            MatchResult matchResult = scan.match();
+            timestamp = Long.parseLong(matchResult.group(0))*1000;
+            scan.close();
+        }
+        return new Date(timestamp);
+    }
 
 }
